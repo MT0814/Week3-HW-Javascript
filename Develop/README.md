fc0bebf9873e7f46947e29ecacb79537f282e50a
millie_tsai <millie_tsai@me.com>
2021-08-20 21:56:42 -0700

    updated js file and README

---
+++ Develop/README.md
@@ -0,0 +1,45 @@
+# 03 JavaScript: Password Generator
+
+## My Task
+
+Applied what I learned and
+
+The password can include special characters. If you’re unfamiliar with these, see this [list of password special characters](https://www.owasp.org/index.php/Password_special_characters) from the OWASP Foundation.
+
+## User Story
+
+```
+AS AN employee with access to sensitive data
+I WANT to randomly generate a password that meets certain criteria
+SO THAT I can create a strong password that provides greater security
+```
+
+## Acceptance Criteria
+
+```
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+```
+
+## Others
+
+![index.html screenshot](./screenshot.png)
+
+[Click me for website](https://mt0814.github.io/Week3-HW-Javascript/)
+
+---
+
+© 2021 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,67 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var numeric = '1234567890';
+var special = '!@#$%^&*_=+-/.?<>)';
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  
+}
+function generatePassword(){
+  var passwordlength = window.prompt ("What is the length for the password? (must between 8 to 128)")
+  if (passwordlength < 8 || passwordlength > 128) {
+    window.alert ('The length must be between 8 and 128.')
+  return;
+  }
+  if (isNaN(parseInt(passwordlength)))
+  {
+  window.alert('Must input numbers.');
+    return false;
+  }
 
+  
+  var character = '';
+  var passwordLowercase = window.confirm ("Do you want lowercase in the password?");
+  var passwordUppercase = window.confirm ("Do you want uppercase in the password?");
+  var passwordNumeric = window.confirm ("Do you want numeric in the password?");
+  var passwordSpecial = window.confirm ("Do you want special characters in the password?");
+  
+  if (passwordLowercase === true) {
+    character = character + lowercase 
+  }
+  if (passwordUppercase === true) {
+    character = character + uppercase 
+  }
+  if (passwordNumeric === true) {
+    character = character + numeric
+  }
+  if (passwordSpecial === true) {
+    character = character + special
+  }
+  if (passwordLowercase === false && passwordUppercase === false && passwordNumeric === false && passwordSpecial === false) {
+    window.alert ("Please pick at least one characters.")
+  return;
+  }
+
+  var password = '';
+
+  for (var i = 0; i < passwordlength; i++) {
+    password += character.charAt (Math.floor(Math.random() * character.length));
+  }
+  return password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
